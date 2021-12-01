d912893f9e70c782ef39d2f4225e59fc2ea1d87d
Nathan Shurtz <n.shurtz@fetchrewards.com>
2021-12-01 13:52:28 -0600

    Day 1 completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,20 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.mapIndexed { index, value ->
+            if (index == 0) false else value > input[index - 1]
+        }.count { it }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        val windowedSums: List<Int> = input.windowed(3).map {it.sum()}
+        return part1(windowedSums)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
