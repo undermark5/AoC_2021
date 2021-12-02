da030fb5457594da95c2d701d7cc159ba0eac3a3
Nathan Shurtz <n.shurtz@fetchrewards.com>
2021-12-01 23:27:10 -0600

    Day 2 both stars

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,15 +1,34 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val forward = input.filter { it.startsWith("forward") }.sumOf { it.split(" ").last().toInt() }
+        val up = input.filter { it.startsWith("up") }.sumOf { it.split(" ").last().toInt() }
+        val down = input.filter { it.startsWith("down") }.sumOf { it.split(" ").last().toInt() }
+        return (forward * (down - up))
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var curHorizontal = 0
+        var curVertical = 0
+        var currentAim = 0
+        input.forEach {
+            val split = it.split(" ")
+            when (split.first()) {
+                "forward" -> {
+                    val num = split.last().toInt()
+                    curHorizontal += num
+                    curVertical += num * currentAim
+                }
+                "down" -> {currentAim += split.last().toInt()}
+                "up" -> {currentAim -= split.last().toInt()}
+            }
+        }
+        return curHorizontal * curVertical
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
 
     val input = readInput("Day02")
     println(part1(input))
